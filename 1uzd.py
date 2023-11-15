9d2c269c469a1caa43991a64e4e9c124fbc6bf27
RenarsGricjus <renars.gricjus@grobinasskola.lv>
2023-11-15 10:51:09 +0200

    pievienoju 1uzd failu

---
+++ 1uzd.py
@@ -0,0 +1,9 @@
+"""
+Izveidot programmu kura prasa lietotājam ievadīt divus skaitļus un aprēķina to summu izvadot terminālī.
+
+
+"""
+a=int(input("Ievadi pirmo ciparu"))
+b=int(input("Ievadi otro skaitli"))
+c= a+b
+print(f"a+b={c}")
